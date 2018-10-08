456a0b58d6c0a89994dadd7e50bbdac44f144e39
Michael <mich561d@hotmail.com>
2018-10-08 11:49:07 +0200

    quick formating update

--- LegoHouse/src/main/java/DBAccess/Connector.java
+++ LegoHouse/src/main/java/DBAccess/Connector.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- The purpose of Connector is to...
-
- @author kasper
+ * The purpose of Connector is to...
+ *
+ * @author kasper
  */
 public class Connector {
 
@@ -17,14 +17,14 @@ public class Connector {
 
     private static Connection singleton;
 
-    public static void setConnection( Connection con ) {
+    public static void setConnection(Connection con) {
         singleton = con;
     }
 
     public static Connection connection() throws ClassNotFoundException, SQLException {
-        if ( singleton == null ) {
-            Class.forName( "com.mysql.cj.jdbc.Driver" );
-            singleton = DriverManager.getConnection( URL, USERNAME, PASSWORD );
+        if (singleton == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            singleton = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         }
         return singleton;
     }
